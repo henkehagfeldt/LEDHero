5ac6aa46fe32b5736ac713bebca8d5094397567f
Henrik Hagfeldt <hagfeldt@student.chalmers.se>
2020-01-23 11:46:14 +0100

    progress

--- hero.py
+++ hero.py
@@ -2,11 +2,66 @@ from __future__ import division
 import time
 
 import pygame
-import hero_tools as ht
+import led_tools as lt
+import mappings
+import evdev
 
+guitar = evdev.InputDevice('/dev/input/event0')
 pygame.init()
 
-ht.init_matrix()
+lt.init_matrix()
 
-ht.set_pixel((0,2))
-ht.set_pixel_clr((0,2), ht.WS_GREEN)
+lt.set_pixel((0,2))
+lt.set_pixel_clr((0,2), lt.WS_GREEN)
+
+current_map = mappings.get_map('hero')
+current_steps = 0
+speed = 2
+
+KEY_STRUM = 0
+KEY_GREEN = 0
+KEY_RED = 0
+KEY_YELLOW = 0
+KEY_BLUE = 0
+KEY_ORANGE = 0
+
+KEYS = [KEY_GREEN, KEY_RED, KEY_YELLOW, KEY_BLUE, KEY_ORANGE]
+
+
+
+# Main Game Loop
+while True:
+
+    # Update LED-Matrix
+    for x in range(0, lt.PXL_CNT/lt.PXL_COL_CNT):
+        for y in range(0, lt.PXL_COL_CNT):
+            # First row, draw new pixels
+            if x == 0:
+                if current_map[x, y + current_steps] > 0:
+                    lt.set_pixel((x, y))
+            else:
+                lt.drop_pixel((x, y))
+
+    # Check Input
+    keys = guitar.active_keys()
+    for(x in range(0, len(KEYS))):
+        if KEYS[x] in keys:
+            lt.button_pixel(x)
+    
+    # Play Sounds
+    
+        
+
+    # Move the map a step, or finish if it's done
+    if current_steps < len(current_map[0]):
+        current_steps += 1
+    else
+        return
+
+    # Update frequency
+    time.sleep(1/speed)
+
+    
+
+
+
---
+++ led_tools.py
@@ -0,0 +1,79 @@
+import Adafruit_WS2801 as ws
+
+dark = 0.4
+WS_GREEN = ws.RGB_to_color(0, 0, 255)
+WS_DGREEN = ws.RGB_to_color(0, 0, int(255*dark))
+WS_RED = ws.RGB_to_color(255, 0, 0)
+WS_RED = ws.RGB_to_color(int(255*dark), 0, 0)
+WS_YELLOW = ws.RGB_to_color(255, 0, 255)
+WS_YELLOW = ws.RGB_to_color(int(255*dark), 0, int(255*dark))
+WS_BLUE = ws.RGB_to_color(0, 255, 0)
+WS_BLUE = ws.RGB_to_color(0, int(255*dark), 0)
+WS_ORANGE = ws.RGB_to_color(255, 0, 60)
+WS_ORANGE = ws.RGB_to_color(int(255*dark), 0, int(60*dark))
+WS_CLEAR = ws.RGB_to_color(0, 0, 0)
+
+PXL_CNT = 40
+PXL_COL_CNT = 8
+
+PXL_CLK = 18
+PXL_DOUT  = 23
+pixels = ws.WS2801Pixels(PXL_CNT, clk=PXL_CLK, do=PXL_DOUT)
+
+led_matrix = []
+
+def init_matrix():
+    direction = 1
+    for x in range(0, PXL_CNT/PXL_COL_CNT):
+            led_matrix.append([])
+            for y in range(0, PXL_COL_CNT):
+                if direction == 1:
+                    led_matrix[x].append(PXL_COL_CNT * x + y)
+                else:
+                    led_matrix[x].append(PXL_COL_CNT * x + PXL_COL_CNT - (y + 1))
+            direction = direction * -1
+
+def get_col_color(x, button):
+    color = WS_CLEAR
+    if button = False:
+        if x == 0:
+            color = WS_GREEN
+        elif x == 1:
+            color = WS_RED
+        elif x == 2:
+            color = WS_YELLOW
+        elif x == 3:
+            color = WS_BLUE
+        elif x == 4:
+            color = WS_ORANGE
+    else:
+        if x == 0:
+            color = WS_DGREEN
+        elif x == 1:
+            color = WS_DRED
+        elif x == 2:
+            color = WS_DYELLOW
+        elif x == 3:
+            color = WS_DBLUE
+        elif x == 4:
+            color = WS_DORANGE
+    return color
+
+def move_pixel((new_x, new_y), (old_x, old_y)):        
+    pixels.set_pixel(led_matrix[old_x][old_y], WS_CLEAR)
+    if new_y >= 0: 
+        pixels.set_pixel(led_matrix[new_x][new_y], get_col_color(new_x, False))
+    pixels.show()
+
+def drop_pixel((x, y)):
+    move_pixel((x, y-1), (x, y))
+
+def set_pixel((x, y)):
+    set_pixel_clr(led_matrix[x][y], get_col_color(x, False))
+
+def set_pixel_clr((x, y), color):
+    pixels.set_pixel(led_matrix[x][y], get_col_color(x, False))
+    pixels.show()
+
+def button_pixel(x):
+    set_pixel_clr((x, PXL_COL_CNT-1), get_col_color(x, button=True))
