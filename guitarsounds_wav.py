f10fd2440671fb5798f7805c1a9304020522de2e
Henrik Hagfeldt <hagfeldt@student.chalmers.se>
2020-03-03 11:00:25 +0100

    changeup

--- guitarsounds_wav.py
+++ guitarsounds_wav.py
@@ -4,23 +4,25 @@ import os
 
 #pygame.mixer.init()
 midi_tones = {}
-
+filetype = 'wav'
+mode = 'Acoustic'
+notes = ['A3', 'A3#_B3b', 'B3', 'C3', 'C3#_D3b', 'D3', 'D3#_E3b', 'E3', 'F3', 'F3#_G3b', 'G3', 'G3#_A3b']
 def init_tones():
-    pygame.mixer.init(44100, -16, 1, 1024)
+    pygame.mixer.init(44100, -16, 1, 512)
     #pygame.mixer.init()
     #pygame.init()
-    midi_tones['a'] = pygame.mixer.Sound('midi/Long/A3.wav')
-    midi_tones['a#'] = pygame.mixer.Sound('midi/Long/A3#_B3b.wav')
-    midi_tones['b'] = pygame.mixer.Sound('midi/Long/B3.wav')
-    midi_tones['c'] = pygame.mixer.Sound('midi/Long/C3.wav')
-    midi_tones['c#'] = pygame.mixer.Sound('midi/Long/C3#_D3b.wav')
-    midi_tones['d'] = pygame.mixer.Sound('midi/Long/D3.wav')
-    midi_tones['d#'] = pygame.mixer.Sound('midi/Long/D3#_E3b.wav')
-    midi_tones['e'] = pygame.mixer.Sound('midi/Long/E3.wav')
-    midi_tones['f'] = pygame.mixer.Sound('midi/Long/F3.wav')
-    midi_tones['f#'] = pygame.mixer.Sound('midi/Long/F3#_G3b.wav')
-    midi_tones['g'] = pygame.mixer.Sound('midi/Long/G3.wav')
-    midi_tones['g#'] = pygame.mixer.Sound('midi/Long/G3#_A3b.wav')
+    midi_tones['a'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[0]+'.'+filetype)
+    midi_tones['a#'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[1]+'.'+filetype)
+    midi_tones['b'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[2]+'.'+filetype)
+    midi_tones['c'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[3]+'.'+filetype)
+    midi_tones['c#'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[4]+'.'+filetype)
+    midi_tones['d'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[5]+'.'+filetype)
+    midi_tones['d#'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[6]+'.'+filetype)
+    midi_tones['e'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[7]+'.'+filetype)
+    midi_tones['f'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[8]+'.'+filetype)
+    midi_tones['f#'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[9]+'.'+filetype)
+    midi_tones['g'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[10]+'.'+filetype)
+    midi_tones['g#'] = pygame.mixer.Sound(filetype+'/'+mode+'/'+notes[11]+'.'+filetype)
 
 
 def play_tone(tone):
