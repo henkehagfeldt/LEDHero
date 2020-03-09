fbd4338f81fd429d67850f5e4e63541bdf3ba82e
Henrik Hagfeldt <hagfeldt@student.chalmers.se>
2020-03-09 19:59:49 +0100

    Update volume.py

--- volume.py
+++ volume.py
@@ -1,8 +1,7 @@
-import alsaaudio
+#!/usr/bin/python3
 import RPi.GPIO as GPIO
 import time
-
-mixer = alsaaudio.Mixer('PCM')
+from subprocess import call
 
 volume_pin = 4
 volume = 0
@@ -12,8 +11,10 @@ GPIO.setup(volume_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     state = GPIO.input(volume_pin) == False
     if state:
-        volume = mixer.getvolume()[0]
-        volume = (volume + 10) % 101
-        mixer.setvolume(volume)
+        #volume = mixer.getvolume()[0]
+        volume = (volume + 10) % 110
+        if volume > 100:
+            volume = 100
+        call(["/usr/bin/amixer", "set", "PCM", str(volume)+"%"])
         print("Setting volume to: "+str(volume))
         time.sleep(0.2)
