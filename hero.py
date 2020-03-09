d8774372e239820de85f8cd1b097d0a492639114
Henrik Hagfeldt <hagfeldt@student.chalmers.se>
2020-03-09 22:33:06 +0100

    Update hero.py

--- hero.py
+++ hero.py
@@ -524,8 +524,6 @@ def freeplay_history():
     for x in range(0, 5):
         for y in range(2, 10):
             if (y - 2) < len(state.history):
-                print("y: "+str(y))
-                print("hi: "+str(state.history_index))
                 if state.history[((state.history_index - 1) - (y - 2)) % 8][x]:
                     lt.set_pixel(x, y)
 
